87cb01b8f873481d3d2942b0c4fd150ae9769515
Kevin ADOSSOU <kevin.adossou@epitech.eu>
2025-10-15 12:12:58 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://backend-yowl-main-jwdqfx.laravel.cloud'
+    'https://yowl-app.vercel.app'
 ],
 
 
